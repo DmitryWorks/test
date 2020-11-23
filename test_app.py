455aa954032a74bd37dfe435be034eb597f79d5f
DmitryWorks <dmt.wrks@gmail.com>
2020-11-23 21:13:34 +0300

    i write it

--- test_app.py
+++ test_app.py
@@ -2,7 +2,10 @@
 #  check info for information
 
 # i tested it, it`s ok
-name = input('name: ')
+name = input('your name: ')
 print('your name is: ', name)
 
 # write this cut for new name pls.
+ surename = input('your sure name: ')
+
+ print('your name is: ', name, ' and sure name is: ', surename, ' Hi ' + name + " " +surename)
