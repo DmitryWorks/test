cd3f9253559c75f61d8904a2f3b93c72e8b392e0
DmitryWorks <dmt.wrks@gmail.com>
2020-11-23 21:26:57 +0300

    to day add birthdate check it

--- test_app.py
+++ test_app.py
@@ -14,3 +14,8 @@ print('your name is: ', name, ' and sure name is: ', surename, ' Hi ' + name + "
 
 # nice work good job i like it
 # write pls next: name sure name birth date and print it, thanks
+
+# ok i write it
+birthdate = input('enter your birth date digits please: ')
+print('Hi dear ', name, 'your Surename is ', surename, ' and birth date is: ', birthdate)
+print('your full id is: ' + name + " " + surename + " " + birthdate)
